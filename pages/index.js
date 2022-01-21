973b3d3492cc68530ac878cd6013c013074f1a9a
Matthew Mellows <matthewmellows@Matthews-MacBook-Pro.local>
2022-01-21 23:31:12 +0000

    removed anything from .next file

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,12 @@ const getAge = (birthDate) => {
   }
 
   // days since the birthdate    
-  var days = Math.floor((now.getTime() - birthDate.getTime())/1000/60/60/24);
+  var days = Math.floor((now.getTime() - birthDate.getTime()) / 1000 / 60 / 60 / 24);
   var age = 0;
   // iterate the years
-  for (var y = birthDate.getFullYear(); y <= now.getFullYear(); y++){
+  for (var y = birthDate.getFullYear(); y <= now.getFullYear(); y++) {
     var daysInYear = isLeap(y) ? 366 : 365;
-    if (days >= daysInYear){
+    if (days >= daysInYear) {
       days -= daysInYear;
       age++;
       // increment the age only if there are available enough days for the year.
@@ -30,7 +30,7 @@ const getAge = (birthDate) => {
   return age;
 }
 
-export const getStaticProps = async() => {
+export const getStaticProps = async () => {
 
   return {
     props: {
@@ -42,8 +42,8 @@ export const getStaticProps = async() => {
 }
 
 export default function Home(props) {
-  
-  const {age, developerAge, professionalAge} = props;
+
+  const { age, developerAge, professionalAge } = props;
 
   return (
     <div>
@@ -58,54 +58,54 @@ export default function Home(props) {
           <Link href="/"><button className="bg-slate-500 border-2 border-slate-500 px-4 py-1 rounded-md font-bold text-white hover:bg-white hover:text-slate-500">123</button></Link>
         </div>
         <div className="basis-96">
-          <img src={me.src} className="w-96"/>
+          <img src={me.src} className="w-96" />
         </div>
       </div>
       <div className="bg-stone-800 p-10 text-white">
         <div className="flex gap-4 mb-5">
-            <div>
-                <Heading tag="h3">What are my skill</Heading>
-                <p>I have more than 8 live projects and can build anything, take a look at the skills I have.</p>
-            </div>
+          <div>
+            <Heading tag="h3">What are my skill</Heading>
+            <p>I have more than 8 live projects and can build anything, take a look at the skills I have.</p>
+          </div>
         </div>
         <div className="grid md:grid-cols-3">
-            <div className="p-0 md:p-4 mb-4">
-                <FontAwesomeIcon icon={faJs} color="#f0db4f" size="2x" className="mb-2" />
-                <Heading tag="h3">Javascript</Heading>
-                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
-            </div>
-            <div className="p-0 md:p-4 mb-4">
-                <FontAwesomeIcon icon={faWordpress} color="#21759b" size="2x" className="mb-2" />
-                <Heading tag="h3">Wordpress</Heading>
-                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
-            </div>
-            <div className="p-0 md:p-4 mb-4">
-                <FontAwesomeIcon icon={faReact} color="#61DBFB" size="2x" className="mb-2" />
-                <Heading tag="h3">React JS</Heading>
-                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
-            </div>
-            <div className="p-0 md:p-4 mb-4">
-                <FontAwesomeIcon icon={faNode} color="#6cc24a" size="2x" className="mb-2" />
-                <Heading tag="h3">React JS and React Native</Heading>
-                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
-            </div>
-            <div className="p-0 md:p-4 mb-4">
-                <FontAwesomeIcon icon={faHtml5} color="#F16529" size="2x" className="mb-2 mr-2" />
-                <FontAwesomeIcon icon={faCss3} color="#264de4" size="2x" className="mb-2" />
-                <Heading tag="h3">HTML5 and CSS3</Heading>
-                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
-            </div>
-            <div className="p-0 md:p-4 mb-4">
-                <FontAwesomeIcon icon={faGoogle} color="#F16529" size="2x" className="mb-2 mr-2" />
-                <Heading tag="h3">Google API Integrations</Heading>
-                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
-            </div>
+          <div className="p-0 md:p-4 mb-4">
+            <FontAwesomeIcon icon={faJs} color="#f0db4f" size="2x" className="mb-2" />
+            <Heading tag="h3">Javascript</Heading>
+            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
+          </div>
+          <div className="p-0 md:p-4 mb-4">
+            <FontAwesomeIcon icon={faWordpress} color="#21759b" size="2x" className="mb-2" />
+            <Heading tag="h3">Wordpress</Heading>
+            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
+          </div>
+          <div className="p-0 md:p-4 mb-4">
+            <FontAwesomeIcon icon={faReact} color="#61DBFB" size="2x" className="mb-2" />
+            <Heading tag="h3">React JS</Heading>
+            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
+          </div>
+          <div className="p-0 md:p-4 mb-4">
+            <FontAwesomeIcon icon={faNode} color="#6cc24a" size="2x" className="mb-2" />
+            <Heading tag="h3">React JS and React Native</Heading>
+            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
+          </div>
+          <div className="p-0 md:p-4 mb-4">
+            <FontAwesomeIcon icon={faHtml5} color="#F16529" size="2x" className="mb-2 mr-2" />
+            <FontAwesomeIcon icon={faCss3} color="#264de4" size="2x" className="mb-2" />
+            <Heading tag="h3">HTML5 and CSS3</Heading>
+            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
+          </div>
+          <div className="p-0 md:p-4 mb-4">
+            <FontAwesomeIcon icon={faGoogle} color="#F16529" size="2x" className="mb-2 mr-2" />
+            <Heading tag="h3">Google API Integrations</Heading>
+            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit malesuada lacus non commodo.</p>
+          </div>
         </div>
       </div>
       <div className="bg-stone-200 block">
-          <div className="grid grid-cols-3">
-            {portfolioItems.map(a => <PortfolioEntry data={a} />)}
-          </div>
+        <div className="grid grid-cols-3">
+          {portfolioItems.map(a => <PortfolioEntry data={a} />)}
+        </div>
       </div>
     </div>
   )
